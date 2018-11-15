cb0732d9b6d927d38baa361f6369ea943a8b8036
Gerardo <gherrer1@binghamton.edu>
2018-11-15 07:23:07 -0500

    So if user searches for a movie and no exact match is found, user will get a list of recommended movies. If user clicks on any of them, that movie is added to the game. Which reminds me - I need a delete functionality in case a user adds the wrong movie

--- app/Components/RTGame.js
+++ app/Components/RTGame.js
@@ -4,7 +4,7 @@ import MovieSearchForm from './MovieSearchForm';
 import MoviesList from './MoviesList';
 import getMovieData from '../api';
 
-const { MOVIE_FOUND, COULD_NOT_FIND_MOVIE_NAMED } = require('../../lambda/messages');
+const { MOVIE_FOUND, COULD_NOT_FIND_MOVIE_NAMED, RECOMMENDATIONS } = require('../../lambda/messages');
 const fakeMovieData = [
     { image: 'https://resizing.flixster.com/yTmdjXPPphGLshRtgVT_uIITjZQ=/fit-in/80x80/v1.bTsxMTMxMTY2NztqOzE3OTUwOzEyMDA7MTAxMDsxMzQ2', name: 'Thor' },
     { name: 'Iron man', image: 'https://resizing.flixster.com/VLupEasUmxg6mJGHLSbuOzw9Sdo=/fit-in/80x80/v1.bTsxMTIxODE4OTtqOzE3OTQ5OzEyMDA7MTAwMDsxNTAw' },
@@ -19,41 +19,79 @@ class RTGame extends React.Component {
 
         this.state = {
             movies: fakeMovieData,
-            errorMessage: '',
-            warningMessage: '',
+            errorMessage: null,
+            warningMessage: null,
+            searchedFor: null,
+            recommendations: null,
         };
 
         this.searchForMovie = this.searchForMovie.bind(this);
+        this.addMovieToGame = this.addMovieToGame.bind(this);
     }
 
     async searchForMovie(movieTitle) {
         const movieData = await getMovieData(movieTitle);
         console.log(movieData);
         if(movieData.message === MOVIE_FOUND) {
-            return this.setState(prevState => ({
-                movies: prevState.movies.concat([ movieData.movie ]),
-                errorMessage: '',
-                warningMessage: '',
-            }));
+            this.addMovieToGame(movieData.movie);
         }
 
         if(movieData.message.startsWith(COULD_NOT_FIND_MOVIE_NAMED)) {
             return this.setState({
                 errorMessage: movieData.message,
-                warningMessage: '',
+                searchedFor: movieData.searchedFor,
+                warningMessage: null,
+                recommendations: null,
+            });
+        }
+
+        if(movieData.message === RECOMMENDATIONS) {
+            return this.setState({
+                errorMessage: null,
+                warningMessage: RECOMMENDATIONS,
+                searchedFor: movieData.searchedFor,
+                recommendations: movieData.recommendations,
             });
         }
     }
 
+    addMovieToGame(movie) {
+        this.setState(prevState => ({
+            movies: prevState.movies.concat([ movie ]),
+            errorMessage: null,
+            warningMessage: null,
+            searchFor: null,
+            recommendations: null,
+        }));
+    }
+
     render() {
-        const { movies, errorMessage } = this.state;
+        const { movies, errorMessage, warningMessage, searchedFor, recommendations } = this.state;
         return (
             <div>
                 <h1>Rotten Tomatoes Game</h1>
                 <MovieSearchForm handleSubmit={this.searchForMovie} />
                 {errorMessage && (
                     <Alert bsStyle="danger">
-                        {COULD_NOT_FIND_MOVIE_NAMED} <strong>{errorMessage.replace(COULD_NOT_FIND_MOVIE_NAMED, '')}</strong>
+                        {COULD_NOT_FIND_MOVIE_NAMED} <strong>{searchedFor}</strong>
+                    </Alert>
+                )}
+                {warningMessage && (
+                    <Alert bsStyle="warning">
+                        <h4>{warningMessage}</h4>
+                        <p>Searched for <strong>{searchedFor}</strong></p>
+                        <ul>
+                            {recommendations.map(movie => (
+                                <li key={movie.image}>
+                                    <a
+                                        className="recommended-title"
+                                        onClick={() => this.addMovieToGame(movie)}
+                                    >
+                                        {movie.name}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
                     </Alert>
                 )}
                 <MoviesList movies={movies} />
