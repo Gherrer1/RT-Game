c15ff3300048237a7f26b79c9f09f2889f6c6a3f
Gerardo <gherrer1@binghamton.edu>
2018-11-17 13:13:55 -0500

    When user clicks enter while on a Player Input, app wont automatically add another Player Input

--- app/Components/PlayersForm.js
+++ app/Components/PlayersForm.js
@@ -3,28 +3,28 @@ import { Button } from 'react-bootstrap/lib';
 import PropTypes from 'prop-types';
 
 function PlayersForm({ players, updatePlayerName, addPlayer }) {
-    return (
-        <React.Fragment>
-            <form onSubmit={e => e.preventDefault()}>
-                {players.map((player, index) => (
-                    <input
-                        className="player-input"
-                        value={player.name}
-                        placeholder={`Player ${index + 1}`}
-                        onChange={e => updatePlayerName(index, e.target.value)}
-                        key={player.id}
-                    />
-                ))}
-                <Button type="submit" onClick={addPlayer} disabled={players.length >= 5}>Add Player</Button>
-            </form>
-        </React.Fragment>
-    );
+	return (
+		<React.Fragment>
+			<form onSubmit={e => e.preventDefault()}>
+				{players.map((player, index) => (
+					<input
+						className="player-input"
+						value={player.name}
+						placeholder={`Player ${index + 1}`}
+						onChange={e => updatePlayerName(index, e.target.value)}
+						key={player.id}
+					/>
+				))}
+				<Button type="button" onClick={addPlayer} disabled={players.length >= 5}>Add Player</Button>
+			</form>
+		</React.Fragment>
+	);
 }
 
 PlayersForm.propTypes = {
-    players: PropTypes.array.isRequired,
-    updatePlayerName: PropTypes.func.isRequired,
-    addPlayer: PropTypes.func.isRequired,
+	players: PropTypes.array.isRequired,
+	updatePlayerName: PropTypes.func.isRequired,
+	addPlayer: PropTypes.func.isRequired,
 };
 
 export default PlayersForm;
