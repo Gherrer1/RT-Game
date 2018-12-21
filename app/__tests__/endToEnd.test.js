135a34e675e0af04887f2aa4b32b23df8e8da35c
Gerardo <gherrer1@binghamton.edu>
2018-12-20 21:25:10 -0500

    Improved readability of endToEnd tests. Tomorrow: GameGrid multiplayer functionality

--- app/__tests__/endToEnd.test.js
+++ app/__tests__/endToEnd.test.js
@@ -79,16 +79,16 @@ describe('end to end tests', () => {
 		it('should not allow `Start Game` until 2+ players and 1+ movies are added to game', async () => {
 			fetch.mockResponseOnce(fakeResponse);
 
-			await page1.type('.player-name-input', 'lonzo');
-			await page1.click('div > button');
+			await typePlayerName(page1, 'lonzo');
+			await clickCreateOrJoinRoom(page1);
 			await page1.waitForSelector('.players-list');
 			let playerNames1 = await page1.$$('.players-list > div');
 			expect(playerNames1.length).toBe(1);
-			const inviteURL = await page1.$eval('.invite-link > a', el => el.href);
+			const inviteURL = await getInviteLink(page1);
 			const page2 = await browser.newPage();
-			await page2.goto(inviteURL);
-			await page2.type('.player-name-input', 'kuzma');
-			await page2.click('div > button');
+			await goToOurSite(page2, inviteURL);
+			await typePlayerName(page2, 'kuzma');
+			await clickCreateOrJoinRoom(page2);
 
 			await page1.waitForSelector('.players-list > div:nth-child(2)');
 			playerNames1 = await page1.$$('.players-list > div');
@@ -102,11 +102,7 @@ describe('end to end tests', () => {
 			let startGameBtnIsDisabled2 = await page2.$('.start-game > button[disabled]') !== null;
 			expect(startGameBtnIsDisabled2).toBe(true);
 
-			await page1.type('.movie-search-form > form > input', 'Saw II');
-			await page1.click('.movie-search-form > form > button');
-			await page1.waitForSelector('.movies-list > div', {
-				timeout: 2000,
-			});
+			await addMovieToGame(page1);
 
 			startGameBtnIsDisabled1 = await page1.$('.link-to-game-grid > button[disabled]') !== null;
 			expect(startGameBtnIsDisabled1).toBe(false);
@@ -141,14 +137,13 @@ describe('end to end tests', () => {
 		it('should not let user join room if room is full', async () => {
 			const pages = await Promise.all((new Array(4).fill(0).map(() => browser.newPage())));
 
-			await page1.type('.player-name-input', 'lonzo');
-			await page1.click('div > button');
-			await page1.waitForSelector('.invite-link > a');
-			const inviteURL = await page1.$eval('.invite-link > a', el => el.href);
+			await typePlayerName(page1, 'lonzo');
+			await clickCreateOrJoinRoom(page1);
+			const inviteURL = await getInviteLink(page1);
 
-			await Promise.all(pages.map(page => page.goto(inviteURL)));
-			await Promise.all(pages.map((page, index) => page.type('.player-name-input', `player ${index}`)));
-			await Promise.all(pages.map(page => page.click('div > button')));
+			await Promise.all(pages.map(page => goToOurSite(page, inviteURL)));
+			await Promise.all(pages.map((page, index) => typePlayerName(page, `Player ${index}`)));
+			await Promise.all(pages.map(page => clickCreateOrJoinRoom(page)));
 
 			const page5 = pages[pages.length - 1];
 			await page5.waitForSelector('.players-list > div:nth-child(5)', {
@@ -156,21 +151,12 @@ describe('end to end tests', () => {
 			});
 
 			const page6 = await browser.newPage();
-			await page6.goto(inviteURL);
+			await goToOurSite(page6, inviteURL);
 
-			const dialogPromise = new Promise((resolve, reject) => {
-				setTimeout(() => reject('never got a dialog'), 4000);
-				page6.on('dialog', (dialog) => {
-					const expectedDialog = 'That room is already full.';
-					if (dialog.message() !== expectedDialog) {
-						return reject(`dialog should have been '${expectedDialog}' but instead was '${dialog.message()}'`);
-					}
-					return resolve();
-				});
-			});
+			const dialogExpectation = createDialogExpectation(page6, 'That room is already full.');
 			await page6.type('.player-name-input', 'player 100');
 			await page6.click('div > button');
-			return dialogPromise;
+			return dialogExpectation;
 		}, 20000);
 	});
 
@@ -181,98 +167,96 @@ describe('end to end tests', () => {
 		let getByText;
 		let container;
 		let queryByText;
-		describe('split screen navigation', () => {
-			beforeEach(() => {
-				renderResult = render(
-					<MemoryRouter>
-						<App />
-					</MemoryRouter>
-				);
-				({ getByText, queryByText, container } = renderResult);
+		beforeEach(() => {
+			renderResult = render(
+				<MemoryRouter>
+					<App />
+				</MemoryRouter>
+			);
+			({ getByText, queryByText, container } = renderResult);
+		});
+
+		it('should start at <GameMode /> route', () => {
+			const selectGameModeNode = container.querySelector('.select-game-mode');
+			expect(selectGameModeNode).toBeTruthy();
+
+			const splitScreenMode = getByText(/Split Screen/);
+			const multiplayerMode = getByText(/Multiplayer/);
+			expect(splitScreenMode).toBeTruthy();
+			expect(multiplayerMode).toBeTruthy();
+		});
+		it('should not show Invite Friends button on <GameSetup /> when user clicks on Split Screen', async () => {
+			expect(container.querySelector('.game-setup')).toBeNull();
+			fireEvent.click(getByText(/Split Screen/));
+			await waitForElement(() => container.querySelector('.game-setup'), {
+				timeout: 500,
+			});
+			const inviteFriendsNode = queryByText('Invite Friends');
+			const joinRoomNode = queryByText('Join Room');
+			expect(inviteFriendsNode).toBeNull();
+			expect(joinRoomNode).toBeNull();
+		});
+		it('should show "Invite Friends" and "Join Room" buttons on <GameSetup /> when user clicks on Multiplayer', async () => {
+			expect(container.querySelector('.game-setup')).toBeNull();
+			fireEvent.click(getByText(/Multiplayer/));
+			await waitForElement(() => container.querySelector('.game-setup'), {
+				timeout: 500,
+			});
+			getByText('Invite Friends');
+			getByText('Join Room');
+		});
+		it('should take you back home when user clicks the Rotten Tomatoes Game navbar', async () => {
+			fireEvent.click(getByText(/Split Screen/));
+			await waitForElement(() => container.querySelector('.game-setup'), {
+				timeout: 100,
+			});
+			expect(queryByText(/Split Screen/)).toBeNull();
+			fireEvent.click(getByText('The Rotten Tomatoes Game'));
+			await waitForElement(() => getByText(/Split Screen/), {
+				timeout: 100,
+			});
+		});
+		it('(Split Screen) should take you from <GameSetup /> -> <GameGrid /> when user clicks Start Game', async () => {
+			fireEvent.click(getByText(/Split Screen/));
+			await waitForElement(() => container.querySelector('.game-setup'), {
+				timeout: 100,
 			});
 
-			it('should start at <GameMode /> route', () => {
-				const selectGameModeNode = container.querySelector('.select-game-mode');
-				expect(selectGameModeNode).toBeTruthy();
-
-				const splitScreenMode = getByText(/Split Screen/);
-				const multiplayerMode = getByText(/Multiplayer/);
-				expect(splitScreenMode).toBeTruthy();
-				expect(multiplayerMode).toBeTruthy();
+			const { getByPlaceholderText } = renderResult;
+			fireEvent.click(getByText('Super heroes'));
+			fireEvent.change(getByPlaceholderText('Player 1'));
+			const startGameButton = getByText('Start Game!');
+			await waitForElement(() => {
+				expect(startGameButton).not.toBeDisabled();
+				return true;
+			}, { timeout: 200 });
+			fireEvent.click(startGameButton);
+			await waitForElement(() => container.querySelector('.game-grid'), {
+				timeout: 200,
 			});
-			it('should not show Invite Friends button on <GameSetup /> when user clicks on Split Screen', async () => {
-				expect(container.querySelector('.game-setup')).toBeNull();
-				fireEvent.click(getByText(/Split Screen/));
-				await waitForElement(() => container.querySelector('.game-setup'), {
-					timeout: 500,
-				});
-				const inviteFriendsNode = queryByText('Invite Friends');
-				const joinRoomNode = queryByText('Join Room');
-				expect(inviteFriendsNode).toBeNull();
-				expect(joinRoomNode).toBeNull();
+		});
+		it('should redirect to home if user navigates to /play without any players or movies state', async () => {
+			renderResult = render(
+				<MemoryRouter
+					initialEntries={['/play']}
+				>
+					<App />
+				</MemoryRouter>
+			);
+			({ getByText, queryByText, container } = renderResult);
+			await waitForElement(() => getByText(/Split Screen/) && getByText(/Multiplayer/), {
+				timeout: 250,
 			});
-			it('should show "Invite Friends" and "Join Room" buttons on <GameSetup /> when user clicks on Multiplayer', async () => {
-				expect(container.querySelector('.game-setup')).toBeNull();
-				fireEvent.click(getByText(/Multiplayer/));
-				await waitForElement(() => container.querySelector('.game-setup'), {
-					timeout: 500,
-				});
-				getByText('Invite Friends');
-				getByText('Join Room');
-			});
-			it('should take you back home when user clicks the Rotten Tomatoes Game navbar', async () => {
-				fireEvent.click(getByText(/Split Screen/));
-				await waitForElement(() => container.querySelector('.game-setup'), {
-					timeout: 100,
-				});
-				expect(queryByText(/Split Screen/)).toBeNull();
-				fireEvent.click(getByText('The Rotten Tomatoes Game'));
-				await waitForElement(() => getByText(/Split Screen/), {
-					timeout: 100,
-				});
-			});
-			it('(Split Screen) should take you from <GameSetup /> -> <GameGrid /> when user clicks Start Game', async () => {
-				fireEvent.click(getByText(/Split Screen/));
-				await waitForElement(() => container.querySelector('.game-setup'), {
-					timeout: 100,
-				});
-
-				const { getByPlaceholderText } = renderResult;
-				fireEvent.click(getByText('Super heroes'));
-				fireEvent.change(getByPlaceholderText('Player 1'));
-				const startGameButton = getByText('Start Game!');
-				await waitForElement(() => {
-					expect(startGameButton).not.toBeDisabled();
-					return true;
-				}, { timeout: 200 });
-				fireEvent.click(startGameButton);
-				await waitForElement(() => container.querySelector('.game-grid'), {
-					timeout: 200,
-				});
-			});
-			it('should redirect to home if user navigates to /play without any players or movies state', async () => {
-				renderResult = render(
-					<MemoryRouter
-						initialEntries={['/play']}
-					>
-						<App />
-					</MemoryRouter>
-				);
-				({ getByText, queryByText, container } = renderResult);
-				await waitForElement(() => getByText(/Split Screen/) && getByText(/Multiplayer/), {
-					timeout: 250,
-				});
-			});
-			it('<GameGrid /> should have a navbar', () => {
-				renderResult = render(
-					<StaticRouter location={preconfiguredRouterLocation} context={{}}>
-						<App />
-					</StaticRouter>
-				);
-				({ container } = renderResult);
-				const navBar = container.querySelector('.site-title');
-				expect(navBar).toBeDefined();
-			});
+		});
+		it('<GameGrid /> should have a navbar', () => {
+			renderResult = render(
+				<StaticRouter location={preconfiguredRouterLocation} context={{}}>
+					<App />
+				</StaticRouter>
+			);
+			({ container } = renderResult);
+			const navBar = container.querySelector('.site-title');
+			expect(navBar).toBeDefined();
 		});
 	});
 });
