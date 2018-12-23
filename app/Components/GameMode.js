8ba9298aa9aea402dec74a982b8b5c65d128eb2c
Gerardo <gherrer1@binghamton.edu>
2018-12-23 11:48:07 -0500

    If a socket exists on global window object when user navigates to / , unsubscribe from all listeners, close the socket, and delete it from window

--- app/Components/GameMode.js
+++ app/Components/GameMode.js
@@ -2,24 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap/lib';
 import NavBar from './NavBar';
+import clearWindowSocket from '../helpers/clearWindowSocket';
 
-function GameMode() {
-	return (
-		<div>
-			<NavBar />
-			<div className="select-game-mode">
-				<Link to="/setup">
-					<Button>Split Screen</Button>
-				</Link>
-				<div />
-				<Link to="/setup-multi">
-					<Button>
-						Multiplayer
-					</Button>
-				</Link>
+class GameMode extends React.Component {
+	componentDidMount() {
+		if (window.socket) {
+			clearWindowSocket(window);
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<NavBar />
+				<div className="select-game-mode">
+					<Link to="/setup">
+						<Button>Split Screen</Button>
+					</Link>
+					<div />
+					<Link to="/setup-multi">
+						<Button>
+							Multiplayer
+						</Button>
+					</Link>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default GameMode;
