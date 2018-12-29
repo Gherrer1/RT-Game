11f3966a2729155e7e38865926f57ca6fcd6cf72
Gerardo <gherrer1@binghamton.edu>
2018-12-29 14:18:39 -0500

    Converted <PlayersForm /> to Typescript

--- app/Components/PlayersForm.tsx
+++ app/Components/PlayersForm.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { Button } from 'react-bootstrap/lib';
 import PropTypes from 'prop-types';
 import PlayerNameInput from './PlayerNameInput';
+import { Player } from '../../sharedTypes';
 
-function PlayersForm({ players, updatePlayerName, addPlayer, removePlayer }) {
+interface Props {
+	players: Player[];
+	updatePlayerName(i: number, s: string): void;
+	removePlayer(i: string): void;
+	addPlayer(): void;
+}
+
+function PlayersForm({ players, updatePlayerName, addPlayer, removePlayer }: Props) {
 	return (
 		<React.Fragment>
 			<form>
